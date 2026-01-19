a9c3b7d3b3d1356b37df2689629ad31cc9c35785
Prabhat Kaushal <prabhatkaushal8910@gmail.com>
2026-01-19 22:58:41 +0530

    new

---
+++ src/Strings.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class Strings {
+    public static void main(Strings[] args){
+        Scanner sc = new Scanner(System.in);
+        String name = sc.nextLine();
+        System.out.println("Enter your name" + " " + name);
+    }
+}
